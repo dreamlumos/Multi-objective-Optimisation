1d158d5c7bd0353c389a7da04bb7907c71c315e0
dreamlumos <30956230+dreamlumos@users.noreply.github.com>
2023-01-21 11:24:25 +0100

    OWA LP with example

---
+++ OWA.py
@@ -0,0 +1,84 @@
+import numpy as np
+
+import gurobipy as gp
+from gurobipy import GRB
+
+# -------- OWA LP -------- #
+
+def OWA_LP(p, n, utilities, weights):
+	"""
+	:param p: nb_items
+	:param n: nb_agents
+	:param utilities: U
+	:param weights: [w_1, w_2, ..., w_n] in order of increasing ordered components (decreasing weights)
+
+	:type nb_items: int
+	:type nb_agents: int
+	:type utilities: ndarray[int]
+	:type weights: ndarray[int]
+
+	:return solution: x
+	:rtype: ndarray[int]
+	"""
+	print(gp.gurobi.version())
+	try:
+
+		# Create a new model
+		m = gp.Model("OWA")
+
+		# Create variables y_1, y_2, ..., y_n
+		y = m.addMVar(shape=n, vtype=GRB.INTEGER, name="y") # TODO: Check if possible and if faster if we use continuous variables here
+
+		# Set objective
+		obj = weights @ y
+		m.setObjective(obj, GRB.MAXIMIZE)
+
+		#### Constraints of the original problem (without linearisation) ####
+
+		# Create binary variables x_ij (if x_ij is 1, the item j is attributed to agent i)
+		x = m.addMVar(shape=(n,p), vtype=GRB.BINARY, name="x")
+
+		# For all agents, we sum the value of the items they are attributed
+		z = m.addMVar(shape=n, vtype=GRB.INTEGER, name="z")
+		m.addConstrs((z[i] == x[i] @ utilities[i] for i in range(n)), name="c_z")
+
+		# We ensure that each item is only attributed once
+		nb_attributions_per_item = np.ones(n)
+		m.addConstrs((sum(x[:,j]) <= nb_attributions_per_item[j] for j in range(p)), name="c_nbattitems")
+
+		# We ensure that each agent receives one item
+		nb_attributions_per_agent = np.ones(n)
+		m.addConstrs((sum(x[i]) <= nb_attributions_per_agent[i] for i in range(n)), name="c_nbattagents")
+
+		#### OWA and linearisation constraints ####
+
+		# Impose order of y_i variables (y_1 <= y_2 <= ... <= y_n)
+		for i in range(1, n):
+			m.addConstr(y[i-1] <= y[i], name="c_y_"+str(i))
+
+		# Calculate value of M to use (has to be larger than any value y_i or z_i could take)
+		M = np.sum(utilities) * 10 
+
+		# Constraints that associate z_i and y_i variables
+		b = m.addMVar(shape=(n,n), vtype=GRB.BINARY, name="b")
+		m.addConstrs((y <= z[k] + M * b[:,k] for k in range(n)), name="c_yz")
+		m.addConstrs((b[:,k] @ np.ones(n) == k for k in range(n)), name="c_b")
+
+		m.write("owa.lp")
+
+		# Optimize model
+		m.optimize()
+
+		print("X: ", x.X)
+		print("Y: ", y.X)
+		print("Z: ", z.X)
+		print("B: ", b.X)
+		print('Obj: %g' % m.objVal)
+
+	except gp.GurobiError as e:
+		print('Error code ' + str(e.errno) + ": " + str(e))
+
+	except AttributeError:
+		print('Encountered an attribute error')
+
+	return x
---
+++ main.py
@@ -0,0 +1,16 @@
+from OWA import *
+from utils import *
+
+
+if __name__ == "__main__":
+
+	print("Multi-objective Optimisation")
+
+	filepath = "given_example.txt"
+	alpha = 1
+
+	nb_items, nb_agents, utilities = parse_OWA_problem(filepath)
+
+	#weights = np.array([1/5, 1/5, 1/5, 1/5, 1/5])
+	weights = OWA_weights_generator(alpha, nb_agents)
+	solution = OWA_LP(nb_items, nb_agents, utilities, weights)
