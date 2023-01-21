84004565f459ed4ef15fd8ff789c7942dd52572f
dreamlumos <30956230+dreamlumos@users.noreply.github.com>
2023-01-21 16:35:05 +0100

    Bug fix + Changed y and z from integer to continuous

--- OWA.py
+++ OWA.py
@@ -5,29 +5,31 @@ from gurobipy import GRB
 
 # -------- OWA LP -------- #
 
-def OWA_LP(n, p, utilities, weights):
+def OWA_LP(n, p, utilities, weights, one_one=False):
 	"""
 	:param n: nb_agents
 	:param p: nb_items
 	:param utilities: U
 	:param weights: [w_1, w_2, ..., w_n] in order of increasing ordered components (decreasing weights)
+	:param one_one: indicates whether only one item is to be attributed per agent
 
 	:type nb_agents: int
 	:type nb_items: int
 	:type utilities: ndarray[int]
 	:type weights: ndarray[int]
+	:type one_one: bool
 
 	:return solution: x
 	:rtype: ndarray[int]
 	"""
-	print(gp.gurobi.version())
+
 	try:
 
 		# Create a new model
 		m = gp.Model("OWA")
 
 		# Create variables y_1, y_2, ..., y_n
-		y = m.addMVar(shape=n, vtype=GRB.INTEGER, name="y") # TODO: Check if possible and if faster if we use continuous variables here
+		y = m.addMVar(shape=n, vtype=GRB.CONTINUOUS, name="y")
 
 		# Set objective
 		obj = weights @ y
@@ -39,16 +41,17 @@ def OWA_LP(n, p, utilities, weights):
 		x = m.addMVar(shape=(n,p), vtype=GRB.BINARY, name="x")
 
 		# For all agents, we sum the value of the items they are attributed
-		z = m.addMVar(shape=n, vtype=GRB.INTEGER, name="z")
+		z = m.addMVar(shape=n, vtype=GRB.CONTINUOUS, name="z")
 		m.addConstrs((z[i] == x[i] @ utilities[i] for i in range(n)), name="c_z")
 
 		# We ensure that each item is only attributed once
-		nb_attributions_per_item = np.ones(n)
+		nb_attributions_per_item = np.ones(p)
 		m.addConstrs((sum(x[:,j]) <= nb_attributions_per_item[j] for j in range(p)), name="c_nbattitems")
 
-		# We ensure that each agent receives one item
-		nb_attributions_per_agent = np.ones(n)
-		m.addConstrs((sum(x[i]) <= nb_attributions_per_agent[i] for i in range(n)), name="c_nbattagents")
+		if one_one:
+			# We ensure that each agent receives only one item
+			nb_attributions_per_agent = np.ones(n)
+			m.addConstrs((sum(x[i]) <= nb_attributions_per_agent[i] for i in range(n)), name="c_nbattagents")
 
 		#### OWA and linearisation constraints ####
 
@@ -81,4 +84,4 @@ def OWA_LP(n, p, utilities, weights):
 	except AttributeError:
 		print('Encountered an attribute error')
 
-	return x
+	return x, m.Runtime
