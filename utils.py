52e133262a32fdf8ece30f92209e8bd9fa8cf3e3
dreamlumos <30956230+dreamlumos@users.noreply.github.com>
2023-01-22 11:18:49 +0100

    utils: Added Choquet parser and Lorenz vector generator

--- utils.py
+++ utils.py
@@ -9,19 +9,35 @@ def read_int(file): return int(read_str(file))
 
 def read_ints(file): return list(map(int, read_str(file).split()))
 
-def parse_OWA_problem(filepath):
+def parse_problem(filepath, problem_type='OWA'):
+    """
+    :param filepath: path to the file containing the problem
+    :param problem_type: OWA / Choquet (case-insensitive)
+    """
 
     with open(filepath) as file:
-        nb_items = read_int(file)
-        nb_agents = read_int(file)
+        
+        nb_objectives = read_int(file)
+        nb_choices = read_int(file)
+        
         utilities = []
-
-        for i in range(nb_agents):
+        for i in range(nb_objectives):
             utilities.append(read_ints(file))
-
         utilities = np.array(utilities)
 
+        costs = []
+        if problem_type.casefold() == 'Choquet'.casefold():
+            costs.append(read_ints(file))
+
+    return nb_objectives, nb_choices, utilities, costs
+
+def parse_OWA_problem(filepath):
+    nb_agents, nb_items, utilities, _ = parse_problem(filepath, problem_type='OWA')
     return nb_agents, nb_items, utilities
+
+def parse_Choquet_problem(filepath):
+    nb_objectives, nb_choices, utilities, costs = parse_problem(filepath, problem_type='Choquet')
+    return nb_objectives, nb_choices, utilities, costs
 
 def generate_OWA_problem(nb_agents, nb_items):
     """
@@ -41,3 +57,13 @@ def OWA_weights_generator(n, alpha=None):
 
     weights = [((n-i+1)/2)**alpha - ((n-i)/n)**alpha for i in range(n)]
     return np.array(weights)
+
+def lorenz_vector(x):
+
+    sorted_x = sorted(x)
+
+    lorenz = []
+    for i in range(len(x)):
+        lorenz.append(sum(sorted_x[:i+1]))
+
+    return lorenz
